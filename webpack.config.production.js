1d1f79b492f70a5606707cbaeacc1a82fbf31179
Abel <esung1129@gmail.com>
2019-04-30 02:02:15 +0900

    :sparkles:webpack production setting

---
+++ webpack.config.production.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    filename: 'static/[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              camelCase: true,
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new ExtractTextPlugin({
+      filename: 'styles/styles.[hash].css',
+      allChunks: true
+    })
+  ]
+};
