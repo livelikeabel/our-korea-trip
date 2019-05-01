f7b205b83dd5bb4b5343b1528be860a4169e66e6
Abel <esung1129@gmail.com>
2019-05-02 01:55:30 +0900

    :sparkles:Search컴포넌트 추가

---
+++ src/components/Search.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {MdSearch} from 'react-icons/md';
+import './Search.scss';
+
+const Search = ({placeholder}) => {
+  return (
+    <div className='Search'>
+      <MdSearch className="Search__icon"/>
+      <input placeholder={placeholder}/>
+    </div>
+  )
+}
+
+Search.defaultProps = {
+  placeholder: '가고 싶은 곳을 검색해보세요!'
+};
+Search.PropTypes =  {
+  placeholder: PropTypes.string
+};
+
+export default Search;
--- src/pages/Main/MainHeader.js
+++ src/pages/Main/MainHeader.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Search from '../../components/Search';
 import './MainHeader.scss';
 
 const MainHeader = () => {
   return (
     <section className="MainHeader">
       <h2>어디 가고 싶으세요?</h2>
+      <div className="MainHeader__search">
+        <Search/>
+      </div>
     </section>
   )
 }
