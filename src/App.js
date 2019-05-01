80934926fad4d32520c19e5ca3c077416212347b
Abel <esung1129@gmail.com>
2019-05-02 00:39:06 +0900

    :art:[#4]페이지 나누기

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './components/Header';
 import Main from './pages/Main';
+import SearchResult from './pages/SearchResult';
+import Detail from './pages/Detail';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header/>
       <Route exact path="/" component={Main} />
+      <Route path="/s" component={SearchResult} />
+      <Route path="/detail" component={Detail}/>
     </BrowserRouter>
   )
 }
