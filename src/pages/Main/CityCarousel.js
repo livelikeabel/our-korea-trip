a68a741261ab5562b9981eb64e7778d39448670d
Abel <esung1129@gmail.com>
2019-05-02 19:06:09 +0900

    :art:cityData추가

--- src/pages/Main/CityCarousel.js
+++ src/pages/Main/CityCarousel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Carousel from '../../components/Carousel';
-import seoul from '../../images/seoul.png';
+import cityData from '../../constants/cityData';
 import './CityCarousel.scss';
 
 const CITY_CAROUSEL_PROPS = {
@@ -14,26 +14,15 @@ const CITY_CAROUSEL_PROPS = {
 const CityCarousel = () => {
   return (
     <Carousel slickProps={CITY_CAROUSEL_PROPS}>
-      <CityCard herf="/" title="서울" img={seoul}/>
-      <CityCard herf="/" title="서울" img={seoul}/>
-      <CityCard herf="/" title="서울" img={seoul}/>
-      <CityCard herf="/" title="서울" img={seoul}/>
-      <CityCard herf="/" title="서울" img={seoul}/>
-      <CityCard herf="/" title="서울" img={seoul}/>
-      <CityCard herf="/" title="서울" img={seoul}/>
-      <CityCard herf="/" title="서울" img={seoul}/>
-      <CityCard herf="/" title="서울" img={seoul}/>
-      <CityCard herf="/" title="서울" img={seoul}/>
-      <CityCard herf="/" title="서울" img={seoul}/>
-      <CityCard herf="/" title="서울" img={seoul}/>
+      {cityData.map(city => <CityCard {...city} key={city.name} />)}
     </Carousel>
   )
 }
 
-const CityCard = ({ herf, img, title }) => (
-  <Link to={herf} className="CityCard" style={{ backgroundImage: `url(${img})` }}>
+const CityCard = ({ href, img, name }) => (
+  <Link to={href} className="CityCard" style={{ backgroundImage: `url(${img})` }}>
     <div className="CityCard__wrapper" />
-    <h3>{title}</h3>
+    <h3>{name}</h3>
   </Link>
 )
 
