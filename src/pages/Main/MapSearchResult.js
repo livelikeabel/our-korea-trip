8ed9e4c9b223d1e8eda3b8c972ffcb9b12ad222c
Abel <esung1129@gmail.com>
2019-05-02 23:02:41 +0900

    :art:지도와 지도검색결과 레이아웃 잡기

---
+++ src/pages/Main/MapSearchResult.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './MapSearchResult.scss';
+
+const MapSearchResult = () => {
+  return <div className="MapSearchResult">결과야</div>
+}
+
+export default MapSearchResult;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import MainHeader from './MainHeader';
+import MainMap from './MainMap';
+import MapSearchResult from './MapSearchResult';
+import './index.scss';
 
 const Main = () => {
   return (
-    <div>
+    <div className="Main">
       <MainHeader />
+      <div className="Main__map-wrapper">
+        <MainMap />
+        <MapSearchResult />
+      </div>
     </div>
   )
 }
