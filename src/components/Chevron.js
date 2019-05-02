01d73ebb2daf7b1c20b256108573dd964ed75335
Abel <esung1129@gmail.com>
2019-05-02 14:16:19 +0900

    :sparkles:Chevron svg 컴포넌트 추가

---
+++ src/components/Chevron.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Chevron.scss';
+
+const pointDirection = {
+  left: 'translate(3.5 12)',
+  right: 'translate(16.5 12) scale(-1 1)'
+};
+
+const svgStyle = {
+  fill: 'none',
+  width: '20px',
+  height: '48px',
+  stroke: 'grey',
+  strokeLinecap: 'round',
+}
+
+const Chevron = ({ direction, customStyle }) => (
+  <svg viewBox="0 0 20 48" style={{...svgStyle, ...customStyle}}>
+    <path
+      d="M12.5 0L0 11.5L12.5 23"
+      transform={pointDirection[direction]}
+    />
+  </svg>
+);
+
+Chevron.displayName = 'Chevron';
+Chevron.propTypes = {
+  customStyle: PropTypes.object,
+  direction: PropTypes.oneOf(['right', 'left']).isRequired
+};
+Chevron.defaultProps = {
+  direction: 'right',
+  customStyle: {}
+};
+export default Chevron;
