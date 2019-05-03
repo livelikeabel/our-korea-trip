90fe87fb1da241eda464eac76e892afc0dcc4cc0
Abel <esung1129@gmail.com>
2019-05-03 15:00:58 +0900

    :sparkles:지도검색결과 캐루셀 추가

--- src/pages/Main/MapSearchResult.js
+++ src/pages/Main/MapSearchResult.js
@@ -1,8 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Carousel from '../../components/Carousel';
 import './MapSearchResult.scss';
+import seoul from '../../images/seoul.png';
+
+const CAROUSEL_PROPS = {
+  autoplay: false,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  row: 2,
+  slidesPerRow: 2
+}
 
 const MapSearchResult = () => {
-  return <div className="MapSearchResult">결과야</div>
+  return (
+    <div className="MapSearchResult">
+      <h2>서울 여행지 추천 해드려요!</h2>
+      <Carousel slickProps={CAROUSEL_PROPS}>
+        <TripInfoCard />
+        <TripInfoCard />
+        <TripInfoCard />
+        <TripInfoCard />
+        <TripInfoCard />
+        <TripInfoCard />
+        <TripInfoCard />
+        <TripInfoCard />
+        <TripInfoCard />
+      </Carousel>
+    </div>
+  )
+}
+
+const TripInfoCard = () => {
+  return (
+    <div className="TripInfoCard">
+      <Link>
+        <img src={seoul} />
+        <div className="TripInfoCard__info">
+          <div className="TripInfoCard__info__tags">
+            <span>서울</span><span>경리단</span>
+          </div>
+          <div className="TripInfoCard__info__title">
+            [제2롯데월드] 하늘과 맞닫는 121층 높이의 잠실타워
+          </div>
+          <p className="TripInfoCard__info__discription">
+            초고층 높이에서 서울의 뷰를 한눈에 볼 수 있는 곳.
+          </p>
+        </div>
+      </Link>
+    </div>
+  )
 }
 
 export default MapSearchResult;
