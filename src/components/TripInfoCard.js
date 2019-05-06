4e11906f15419ba2c328ca31f2561c24c379d046
Abel <esung1129@gmail.com>
2019-05-06 22:33:54 +0900

    :construction:WIP..

--- src/components/TripInfoCard.js
+++ src/components/TripInfoCard.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import './TripInfoCard.scss';
 
 const TripInfoCard = ({tags, img, title, discription}) => {
+
+  // const imgEl = useRef();
+
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver ((
+  //     entries => {
+  //       entries.forEach(entry => {
+  //         const { isIntersecting } = entry;
+
+  //         if (isIntersecting) {
+  //           observer.src = 
+  //         }
+  //       })
+  //     }
+  //   ) => {
+
+  //   },
+  //   {
+  //     // root라는 키로 컨테이너 ref를 넘겨 주어야 할 것 같다...
+  //     // root: document.querySelector(".container") ??
+  //   });
+
+  //   observer.observe(imgEl)
+  // }, []);
+
   return (
     <div className="TripInfoCard">
       <Link to="/">
+        {/* <img ref={imgEl} src={img} /> */}
         <img src={img} />
         <div className="TripInfoCard__info">
           <div className="TripInfoCard__info__tags">
--- src/constants/seoulMockData.js
+++ src/constants/seoulMockData.js
@@ -10,24 +10,24 @@ export default [
   },
   {
     _id: 2,
-    tags: ['서울', '잠실', '롯데월드타워'],
-    img: seoul,
-    title: '[제2롯데월드] 하늘과 맞닫는 121층 높이의 잠실타워',
-    discription: '초고층 높이에서 서울의 뷰를 한눈에 볼 수 있는 곳.'
+    tags: ['서울', '한강', '63빌딩'],
+    img: 'https://cdn.pixabay.com/photo/2014/08/05/09/37/seoul-410266_960_720.jpg',
+    title: '[63빌딩] 원조는 나야! 서울의 상징적인 타워.',
+    discription: ' 63층 높이에서 한강 뷰를 한눈에 볼 수 있는 곳.'
   },
   {
     _id: 3,
-    tags: ['서울', '잠실', '롯데월드타워'],
-    img: seoul,
-    title: '[제2롯데월드] 하늘과 맞닫는 121층 높이의 잠실타워',
+    tags: ['서울', '광화문'],
+    img: 'https://cdn.pixabay.com/photo/2015/02/14/08/26/gwanghwamun-636113_960_720.jpg',
+    title: '[광화문] 선조들의 귀한 유산',
     discription: '초고층 높이에서 서울의 뷰를 한눈에 볼 수 있는 곳.'
   },
   {
     _id: 4,
-    tags: ['서울', '잠실', '롯데월드타워'],
-    img: seoul,
-    title: '[제2롯데월드] 하늘과 맞닫는 121층 높이의 잠실타워',
-    discription: '초고층 높이에서 서울의 뷰를 한눈에 볼 수 있는 곳.'
+    tags: ['서울', '동대문', 'DDP'],
+    img: "https://cdn.pixabay.com/photo/2015/01/29/15/57/modern-616312_960_720.jpg",
+    title: '[동대문 디자인 플라자] 서울의 새로운 디자인 상징',
+    discription: 'UFO 닮았습니다. 레드닷 어워즈에서 상을 탔죠!'
   },
   {
     _id: 5,
